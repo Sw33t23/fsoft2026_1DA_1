e2c4fddfd429b1aa241a2a133fb6ab8cb48dca70
DMSREIS <diogovreis5@gmail.com>
2026-06-01 17:23:25 +0100

    Add RankingView class header file

---
+++ GamesPlatform/View/RankingView.h
@@ -0,0 +1,19 @@
+#ifndef RANKING_VIEW_H
+#define RANKING_VIEW_H
+ 
+#include <string>
+#include <vector>
+#include "RankingModel.h"
+ 
+class RankingView {
+public:
+    void exibirTop10(const std::vector<EntradaRanking>& lista,
+                     const std::string& nomeJogo,
+                     const std::string& colunaValor);
+
+    int pedirOpcaoMenu();
+ 
+    void mostrarOpcaoInvalida();
+    void mostrarAVoltar();
+};
+#endif
