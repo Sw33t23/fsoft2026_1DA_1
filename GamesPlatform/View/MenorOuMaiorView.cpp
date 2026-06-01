f4fec24383ae0ded577dd24439627d7f9f9f0feb
real-andrek <129605179+real-andrek@users.noreply.github.com>
2026-06-01 12:04:10 +0100

    Atualizar View MenorOuMaiorView.cpp

---
+++ GamesPlatform/View/MenorOuMaiorView.cpp
@@ -0,0 +1,17 @@
+//
+// Created by andre on 01/06/2026.
+//
+
+#include "MenorOuMaiorView.h"
+#include <iostream>
+
+using namespace std;
+
+void MenorOuMaiorView::mostrarInstrucoes() const
+{
+    cout << "\n========================================" << endl;
+    cout << "           MAIOR OU MENOR                 " << endl;
+    cout << "==========================================" << endl;
+    cout << "REGRAS:" << endl;
+
+}
