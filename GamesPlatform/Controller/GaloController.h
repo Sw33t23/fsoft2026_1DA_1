348d3c67a61c7be9f43fcb752cccaf5605580779
Diogo <drm2004.dm93@gmail.com>
2026-06-01 11:53:38 +0100

    Adicionar View e controller do jogo do galo

---
+++ GamesPlatform/Controller/GaloController.h
@@ -0,0 +1,13 @@
+//
+// Created by drm20 on 01/06/2026.
+//
+
+#ifndef GITIGNORE_GALOCONTROLLER_H
+#define GITIGNORE_GALOCONTROLLER_H
+
+
+class GaloController {
+};
+
+
+#endif //GITIGNORE_GALOCONTROLLER_H
