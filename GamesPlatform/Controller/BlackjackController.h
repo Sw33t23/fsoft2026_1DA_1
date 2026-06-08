509ca4eae9657bf55a0280f55b1aa4a3173a206d
Sw33t23 <goncalopixadado@gmail.com>
2026-06-08 10:37:34 +0100

    Update BlackjackController.h

--- GamesPlatform/Controller/BlackjackController.h
+++ GamesPlatform/Controller/BlackjackController.h
@@ -3,15 +3,18 @@
 
 #include "../Model/blackjack.h"
 #include "../View/BlackjackView.h"
+#include "../Model/RankingModel.h"
+#include <string>
 
 class BlackjackController {
 private:
     Blackjack model;
     BlackjackView view;
+    std::string playerUsername;
 
 public:
-    BlackjackController(double startingBalance);
-    void playBlackjackRound(double& userBalance);
+    BlackjackController(std::string username, double startingBalance);
+    void playBlackjackRound(double& userBalance, RankingModel& ranking);
 };
 
 #endif
