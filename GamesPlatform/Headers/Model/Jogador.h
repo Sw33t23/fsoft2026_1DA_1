0c647f0608d735c0a18de94e1f48857a67e93b8d
real.andrek <129605179+real-andrek@users.noreply.github.com>
2026-06-09 14:37:57 +0100

    Add Jogador.h

---
+++ GamesPlatform/Headers/Model/Jogador.h
@@ -0,0 +1,28 @@
+//
+// Created by andre on 01/06/2026.
+//
+
+#ifndef JOGADOR_H
+#define JOGADOR_H
+#include <string>
+
+using namespace std;
+
+struct Jogador
+{
+    string username;
+    string password;
+    double saldo = 1000.0;
+    int leastTrys = 10000;
+
+    void setLeastTrys(int tentativas)
+    {
+        if (tentativas < leastTrys)
+        {
+            leastTrys = tentativas;
+        }
+    }
+};
+
+
+#endif //JOGADOR_H
