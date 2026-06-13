343b3a14025f37c21844687a14e21efe1236d5bb
Sw33t23 <goncalopixadado@gmail.com>
2026-06-13 11:20:49 +0100

    Update Ranking.cpp

---
+++ GamesPlatform/Source/Model/Ranking.cpp
@@ -0,0 +1,108 @@
+#include "../../Headers/Model/Ranking.h"
+
+const int TOP_MAX = 10;
+
+void Ranking::atualizarBlackjack(std::string username, int saldo) {
+    bool encontrado = false;
+
+    for (int i = 0; i < (int)rankingBlackjack.size(); i++) {
+        if (rankingBlackjack[i].username == username) {
+            encontrado = true;
+            if (saldo > rankingBlackjack[i].valor) {
+                rankingBlackjack[i].valor = saldo;
+            }
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        EntradaRanking novo;
+        novo.username = username;
+        novo.valor = saldo;
+        rankingBlackjack.push_back(novo);
+    }
+
+    for (int i = 0; i < (int)rankingBlackjack.size() - 1; i++) {
+        for (int j = 0; j < (int)rankingBlackjack.size() - 1 - i; j++) {
+            if (rankingBlackjack[j].valor < rankingBlackjack[j+1].valor) {
+                EntradaRanking temp  = rankingBlackjack[j];
+                rankingBlackjack[j]  = rankingBlackjack[j+1];
+                rankingBlackjack[j+1]= temp;
+            }
+        }
+    }
+
+    if ((int)rankingBlackjack.size() > TOP_MAX) {
+        rankingBlackjack.pop_back();
+    }
+}
+
+void Ranking::atualizarGalo(std::string username, int vitorias_consecutivas) {
+    bool encontrado = false;
+
+    for (int i = 0; i < (int)rankingGalo.size(); i++) {
+        if (rankingGalo[i].username == username) {
+            encontrado = true;
+            if (vitorias_consecutivas > rankingGalo[i].valor) {
+                rankingGalo[i].valor = vitorias_consecutivas;
+            }
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        EntradaRanking nova;
+        nova.username = username;
+        nova.valor = vitorias_consecutivas;
+        rankingGalo.push_back(nova);
+    }
+
+    for (int i = 0; i < (int)rankingGalo.size() - 1; i++) {
+        for (int j = 0; j < (int)rankingGalo.size() - 1 - i; j++) {
+            if (rankingGalo[j].valor < rankingGalo[j+1].valor) {
+                EntradaRanking temp = rankingGalo[j];
+                rankingGalo[j]   = rankingGalo[j+1];
+                rankingGalo[j+1] = temp;
+            }
+        }
+    }
+
+    if ((int)rankingGalo.size() > TOP_MAX) {
+        rankingGalo.pop_back();
+    }
+}
+
+void Ranking::atualizarMaiorMenor(std::string username, int tentativas) {
+    bool encontrado = false;
+
+    for (int i = 0; i < (int)rankingMaiorMenor.size(); i++) {
+        if (rankingMaiorMenor[i].username == username) {
+            encontrado = true;
+            if (tentativas < rankingMaiorMenor[i].valor) {
+                rankingMaiorMenor[i].valor = tentativas;
+            }
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        EntradaRanking nova;
+        nova.username = username;
+        nova.valor = tentativas;
+        rankingMaiorMenor.push_back(nova);
+    }
+
+    for (int i = 0; i < (int)rankingMaiorMenor.size() - 1; i++) {
+        for (int j = 0; j < (int)rankingMaiorMenor.size() - 1 - i; j++) {
+            if (rankingMaiorMenor[j].valor > rankingMaiorMenor[j+1].valor) {
+                EntradaRanking temp   = rankingMaiorMenor[j];
+                rankingMaiorMenor[j]  = rankingMaiorMenor[j+1];
+                rankingMaiorMenor[j+1]= temp;
+            }
+        }
+    }
+
+    if ((int)rankingMaiorMenor.size() > TOP_MAX) {
+        rankingMaiorMenor.pop_back();
+    }
+}
